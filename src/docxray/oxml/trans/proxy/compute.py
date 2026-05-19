2ce87f2636a3f0278bfac2ebc8e6299de0d95433
medivh <dsd2@dsd.ru>
2026-05-19 22:00:14 +0500

    refac compute module

--- src/docxray/oxml/trans/proxy/compute.py
+++ src/docxray/oxml/trans/proxy/compute.py
@@ -5,7 +5,10 @@ from typing import cast
 from docxray.constants import PCT_TO_PERCENT_RATIO
 from docxray.oxml.trans.shared import CT_OnOff, CT_TblWidth
 from docxray.oxml.trans.st.enums import SE_OnOff1, SE_TblWidth
-from docxray.oxml.trans.st.shared_common import ST_UniversalMeasure
+from docxray.oxml.trans.st.shared_common import (
+    ST_PositiveUniversalMeasure,
+    ST_UniversalMeasure,
+)
 from docxray.xsd.facets import PatternFacet
 
 from .shared import Cm, Inches, Length, Mm, NotFound, Pica, Pt, Twips
@@ -37,7 +40,7 @@ def width(
     ):
         return None
     if isinstance(width_elm.w, str):
-        return width_string(width_elm.w)
+        return universal_measure(width_elm.w)
     width_float = float(width_elm.w)
     if width_elm.type == SE_TblWidth.TWIPS:
         return Twips(width_float)
@@ -46,24 +49,65 @@ def width(
     return normalize_pct(width_float)
 
 
-def width_string(width: str) -> Length | None:
-    """Parse width string to `Length` instance.
+def on_off(on_off: NotFound | None | bool | SE_OnOff1 | CT_OnOff) -> bool:
+    def _val(val: None | bool | SE_OnOff1) -> bool:
+        if val is None:
+            return True
+        if isinstance(val, bool):
+            return val
+        if isinstance(val, SE_OnOff1):
+            if val == SE_OnOff1.ON:
+                return True
+            return False
 
-    Normally XSD module before will validate string so we return
-    `None` silently.
+    if isinstance(on_off, NotFound):
+        return False
+    if isinstance(on_off, CT_OnOff):
+        return _val(on_off.val)
+    return _val(on_off)
+
+
+def signed_twips_measure(twips: int | str) -> Length:
+    if isinstance(twips, int):
+        return Twips(twips)
+    return universal_measure(twips)
+
+
+def twips_measure(twips: int | str) -> Length:
+    if isinstance(twips, int):
+        return Twips(twips)
+    return universal_measure(
+        twips,
+        cast(
+            "PatternFacet", ST_PositiveUniversalMeasure.FACETS["pattern"]
+        ).value,
+    )
+
+
+def universal_measure(val: str, override_pattern: str | None = None) -> Length:
+    """Parse string to `Length` instance.
 
     Args:
-        width (str): Width string from any width_elm.
+        val (str): Any length measure in Word.
+        override_pattern (str): Pattern of another universal measure,
+            e.g. signed version or whatever.
+
+    Raises:
+        ValueError: If cannot parse measure.
 
     Returns:
-        Length | None: Number instance or it's not parsed.
+        Length: Numeric instance.
     """
-    pattern = cast("PatternFacet", ST_UniversalMeasure.FACETS["pattern"]).value
+    pattern = (
+        override_pattern
+        or cast("PatternFacet", ST_UniversalMeasure.FACETS["pattern"]).value
+    )
+    err = ValueError(f"Measure cannot be identified for `{val}`")
     if pattern is None:
-        return None
-    match = re.compile(pattern).fullmatch(width.strip())
+        raise err
+    match = re.compile(pattern).fullmatch(val.strip())
     if not match:
-        return None
+        raise err
 
     integer_part = match.group(1)
     decimal_part = match.group(2)
@@ -84,22 +128,4 @@ def width_string(width: str) -> Length | None:
         case "pc" | "pi":
             return Pica(value)
         case _:
-            return None
-
-
-def on_off(on_off: NotFound | None | bool | SE_OnOff1 | CT_OnOff) -> bool:
-    def _val(val: None | bool | SE_OnOff1) -> bool:
-        if val is None:
-            return True
-        if isinstance(val, bool):
-            return val
-        if isinstance(val, SE_OnOff1):
-            if val == SE_OnOff1.ON:
-                return True
-            return False
-
-    if isinstance(on_off, NotFound):
-        return False
-    if isinstance(on_off, CT_OnOff):
-        return _val(on_off.val)
-    return _val(on_off)
+            raise err
