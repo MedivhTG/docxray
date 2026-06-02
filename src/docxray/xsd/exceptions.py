79e4c191ea4d11a72e6e940d57e932273ed760ec
Nalobin Slava <dsd2@dsd.ru>
2026-06-02 16:43:17 +0500

    docstrings xsd

--- src/docxray/xsd/exceptions.py
+++ src/docxray/xsd/exceptions.py
@@ -4,12 +4,22 @@ from typing import Any, Self
 
 
 class XsdBaseError(Exception):
-    pass
+    """Standard error for XSD validation."""
 
 
 class XsdTypeError(XsdBaseError):
     @classmethod
     def construct(cls, obj: Any, valid_type: type, extra: str = "") -> Self:
+        """Cosntruct standard XSD error for raise.
+
+        Args:
+            obj (Any): Given object for repesentation.
+            valid_type (type): XSD type validation cls.
+            extra (str, optional): Extra info for error. Defaults to "".
+
+        Returns:
+            Self: Constructed `XsdTypeError`
+        """
         msg = f"Invalid XSD type for object `{obj}` of type `{obj.__class__.__name__}`, expected `{valid_type.__name__}`"
         if extra:
             msg += f": {extra}"
